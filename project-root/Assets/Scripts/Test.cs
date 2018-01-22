0f3e92b251ab15015393a1720e1254915a807412
Connor Botts <connor.botts@gmail.com>
2018-01-22 00:23:35 -0500

    Set up Visual Studio solution and added a .editorconfig for style guide

---
+++ project-root/Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start ()
+    {
+        var go = new GameObject();
+    }
+
+    private void Heck ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+}
