20af9b3269adafc50665baf411255bf9fb49d915
Erick Eiben <eeiben1@student.fitchburgstate.edu>
2018-02-15 12:03:58 -0500

    Reorganized the assets folder to be design proof

--- project-root/Assets/Scripts/Character.cs
+++ project-root/Assets/Scripts/Character.cs
@@ -1,21 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using InControl;
 
 namespace CharacterScripts
 {
-    public abstract class Character : MonoBehaviour, IDamageable<float>
+    public abstract class Character : MonoBehaviour, IDamageable<int, int>, IHealth<int>
     {
-        public virtual void TakeDamage(float amount)
+        public int StartingHealth
         {
+            get
+            {
+                return startingHealth;
+            }
 
+            set
+            {
+                startingHealth = value;
+            }
         }
 
-        public virtual void DealDamage(float amount)
+        public int MaxHealth
         {
+            get
+            {
+                return maxHealth;
+            }
 
+            set
+            {
+                maxHealth = value;
+            }
+        }
+
+        public int CurrentHealth
+        {
+            get
+            {
+                return currentHealth;
+            }
+
+            set
+            {
+                currentHealth = value;
+            }
+        }
+
+        private int startingHealth;
+        private int maxHealth;
+        private int currentHealth;
+
+        public virtual void SetStartingHealth(int amount)
+        {
+            startingHealth = amount;
+        }
+
+        public virtual void SetMaxHealth(int amount)
+        {
+            maxHealth = amount;
+        }
+
+        public virtual void SetCurrentHealth(int amount)
+        {
+            currentHealth = amount;
+        }
+
+        public virtual void DealDamage(int targetHealthValue, int amount)
+        {
+            targetHealthValue -= amount;
+        }
+
+        public virtual void TakeDamage(int healthValue, int amount)
+        {
+            healthValue -= amount;
         }
     }
 }
-
