89b7012f37bcb32dd1d9ccea7d8b942572714124
Randy <randy331996@gmail.com>
2018-02-20 11:24:26 -0500

    Added Editor folder, and added NavSpawnerEditor.cs to the Editor folder

---
+++ project-root/Assets/Editor/MonsterspawnerEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(Monsterspawner))]
+public class MonsterspawnerEditor : Editor
+{
+    
+   
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        EditorGUILayout.HelpBox("click the check box of the monster you would like to spawn and type the amount you would like to spawn then when you want it to respawn under time to spawn", MessageType.Info);
+        Monsterspawner mySpawner = (Monsterspawner)target;
+        if (GUILayout.Button("Spawn Wolf"))
+        {
+            mySpawner.MakeWolf();
+        }
+    }
+}
