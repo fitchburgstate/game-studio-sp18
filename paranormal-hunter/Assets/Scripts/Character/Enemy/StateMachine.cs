438f9416da413079d9aaec6f23bdd49955c7ecb8
Erick Eiben <eeiben1@student.fitchburgstate.edu>
2018-02-21 14:24:53 -0500

    Further work on AI state machine, saving progress to remote repo

--- paranormal-hunter/Assets/Scripts/Character/Enemy/StateMachine.cs
+++ paranormal-hunter/Assets/Scripts/Character/Enemy/StateMachine.cs
@@ -31,11 +31,11 @@ public abstract class State : ScriptableObject
 
             if (decisionMade)
             {
-                // Transition to a true state
+                controller.TransitionToState(transitions[i].trueState);
             }
             else
             {
-                // Transition to a false state
+                controller.TransitionToState(transitions[i].falseState);
             }
         }
     }
@@ -49,6 +49,96 @@ public abstract class Action : ScriptableObject
 public abstract class Decision : ScriptableObject
 {
     public abstract bool Decide(Enemy controller);
+}
+
+public class WanderAction : Action
+{
+    public override void Act(Enemy controller)
+    {
+        Wander(controller);
+    }
+
+    private void Wander(Enemy controller)
+    {
+        // Wander mechanics go here
+    }
+}
+
+public class IdleAction: Action
+{
+    public override void Act(Enemy controller)
+    {
+        Idle(controller);
+    }
+
+    private void Idle(Enemy controller)
+    {
+        // Idle mechanics go here
+    }
+}
+
+public class ChaseAction : Action
+{
+    public override void Act(Enemy controller)
+    {
+        Chase(controller);
+    }
+
+    private void Chase(Enemy controller)
+    {
+        // Chase mechanics go here
+    }
+}
+
+public class DashAction : Action
+{
+    public override void Act(Enemy controller)
+    {
+        Dash(controller);
+    }
+
+    private void Dash(Enemy controller)
+    {
+        // Chase mechanics go here
+    }
+}
+
+public class AttackAction : Action
+{
+    public override void Act(Enemy controller)
+    {
+        Attack(controller);
+    }
+
+    private void Attack(Enemy controller)
+    {
+        // Attack mechanics go here
+    }
+}
+
+public class ScanDecision : Decision
+{
+    public override bool Decide(Enemy controller)
+    {
+        var noEnemyFound = Scan(controller);
+        return noEnemyFound;
+    }
+
+    private bool Scan(Enemy controller)
+    {
+        // Scan mechanics go here
+        return false; // This must be changed later
+    }
+}
+
+public class ActiveStateDecision : Decision
+{
+    public override bool Decide(Enemy controller)
+    {
+        // Active State Decision mechanics go here
+        var hasTarget = false;
+        return hasTarget;
+    }
 }
 
 [System.Serializable]
