18505602953421d32bb5f9bc339ac1b263e623c3
Randy <randy331996@gmail.com>
2018-02-22 11:59:38 -0500

    Merged development into monster-spawner-branch, fixed conflicts

---
+++ paranormal-hunter/Assets/Editor/NavSpawnerEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NavMeshSpawner))]
+public class NavSpawnerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        NavMeshSpawner mySpawner = (NavMeshSpawner)target;
+        if (GUILayout.Button("Spawn Monster"))
+        {
+            mySpawner.MakeWolf();
+        }
+    }
+
+
+}
