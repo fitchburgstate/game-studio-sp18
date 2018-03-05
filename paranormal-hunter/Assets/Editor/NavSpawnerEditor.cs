6c0bc801defc28ecf8ca3e7cf580119cc3196cc9
Randy <randy331996@gmail.com>
2018-03-05 16:34:51 -0500

    forgot to add navmeshspawner to my last commit

--- paranormal-hunter/Assets/Editor/NavSpawnerEditor.cs
+++ paranormal-hunter/Assets/Editor/NavSpawnerEditor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(NavMeshSpawner))]
+[CustomEditor(typeof(MonsterSpawner))]
 public class NavSpawnerEditor : Editor
 {
     public override void OnInspectorGUI()
@@ -11,7 +11,7 @@ public class NavSpawnerEditor : Editor
         DrawDefaultInspector();
         EditorGUILayout.HelpBox("Drag your monster prefab onto the moster list to make it spawn. If 'Amount To Spawn' is 1 then it will spawn at origin point. If not it will spawn randomly on the Nav Mesh", MessageType.Info);
         EditorGUILayout.HelpBox("Spawn Monsters button spawns a monster at the origin of the spawn point when you drag the monster prefab into Monster To Spawn", MessageType.Info);
-        NavMeshSpawner mySpawner = (NavMeshSpawner)target;
+        MonsterSpawner mySpawner = (MonsterSpawner)target;
         if (GUILayout.Button("Spawn Monster"))
         {
             mySpawner.MakeWolf();
