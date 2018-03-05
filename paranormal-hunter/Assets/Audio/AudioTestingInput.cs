410153f0b3e82cce1ac3d8ba27f18085db9370c0
Ethan Thibault <wickedlystudios@gmail.com>
2018-03-05 15:30:20 -0500

    Placeholder Player Hit
    - Modulating pitch via Fabric
    - Volume managed by Fabric as well

--- paranormal-hunter/Assets/Audio/AudioTestingInput.cs
+++ paranormal-hunter/Assets/Audio/AudioTestingInput.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioTestingInput : MonoBehaviour {
+	[Header("Test Inputs")]
 	public KeyCode Footstep = KeyCode.F;
+	public KeyCode PlayerHit = KeyCode.H;
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(Footstep)){
 			PlayFootsteps();
 		}
+		if(Input.GetKeyDown(PlayerHit)){
+			PlayPlayerHit();
+		}
 	}
+	// Player footsteps
+	/* In the future will create overrides within character controller
+		for differing stuff. Also do enemies need steps? */
 	void PlayFootsteps(){
 		Fabric.EventManager.Instance.PostEvent("Footstep");
+	}
+	// For when the player gets hit
+	void PlayPlayerHit(){
+		Fabric.EventManager.Instance.PostEvent("Player Hit");
 	}
 }
