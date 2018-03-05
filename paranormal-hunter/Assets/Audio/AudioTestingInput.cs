389ddf2b4ddcbf23da3bb2097bc41d935c58748a
Ethan Thibault <wickedlystudios@gmail.com>
2018-03-05 13:25:20 -0500

    Placeholder Footsteps, Audio Testing Input Script
    - Created script for testing audio input in my scene, includes methods that Connor can use later to post events
    - Added 7 footstep sounds, might be too many
    - Implemented them through random container, must remember to always override the 3D settings.

---
+++ paranormal-hunter/Assets/Audio/AudioTestingInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTestingInput : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F)){
+			PlayFootsteps();
+		}
+	}
+	void PlayFootsteps(){
+		Fabric.EventManager.Instance.PostEvent("Footstep");
+	}
+}
