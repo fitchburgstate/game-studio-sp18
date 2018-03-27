4d8c388c86c29e88a7ee65e21c1c6802615d5391
Ethan Thibault <wickedlystudios@gmail.com>
2018-03-27 16:58:43 -0400

    Dynamic Music Switches Implemented
    - Two events created that handle switching the music from Exploration to Combat and vice versa, just need to be called with a line of code.
    - Event List Window allows me to create dynamic events where one event code calls a bunch of things - using the same for the Luger shot

--- paranormal-hunter/Assets/Audio/AudioTestingInput.cs
+++ paranormal-hunter/Assets/Audio/AudioTestingInput.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioTestingInput : MonoBehaviour {
-	
+	[Header("Test Inputs - Music")]
+	public KeyCode MusicStop = KeyCode.Q;
 	[Header("Test Inputs - Player")]
 	public KeyCode Footstep = KeyCode.F;
 	public KeyCode PlayerHit = KeyCode.H;
@@ -14,8 +15,23 @@ public class AudioTestingInput : MonoBehaviour {
 	public KeyCode PlayerLugerShot = KeyCode.L;
 	[Header("Test Inputs - Mob")]
 	public KeyCode WolfAttack = KeyCode.W;
+
+	/* Private */
+	bool ExpoMusicPlaying = true;
 	
 	void Update () {
+
+		/* Music Events */
+		if(Input.GetKeyDown(MusicStop)){
+			if(ExpoMusicPlaying == false){
+				CombattoExpoMusic();
+				ExpoMusicPlaying = true;
+			}
+			else {
+				ExpotoCombatMusic();
+				ExpoMusicPlaying = false;
+			}
+		}
 		
 		/* Player Events */
 		if(Input.GetKeyDown(Footstep)){
@@ -44,6 +60,7 @@ public class AudioTestingInput : MonoBehaviour {
 		}
 	}
 	
+	/***** SFX Events *****/
 	/* All SFX should be posted with a reference to their parent object,
 		So that Fabric can track their 3D position and instances.
 		If this isn't possible, just remove the ", gameObject" */
@@ -77,5 +94,18 @@ public class AudioTestingInput : MonoBehaviour {
 	void PlayWolfAttack(){
 		// Currently has 1 attack modulated multiple times, will fix later
 		Fabric.EventManager.Instance.PostEvent("Wolf Attack", gameObject);
+	}
+
+	/***** Music Events *****/
+	// Music Events don't need to pass a gameObject because they are entirely 2D.
+
+	// Transitions music from Exploration to Combat
+	void ExpotoCombatMusic(){
+		Fabric.EventManager.Instance.PostEvent("Expo to Combat Music");
+	}
+	
+	//Transitions music from Combat to Exploration 
+	void CombattoExpoMusic(){
+		Fabric.EventManager.Instance.PostEvent("Combat to Expo Music");
 	}
 }
