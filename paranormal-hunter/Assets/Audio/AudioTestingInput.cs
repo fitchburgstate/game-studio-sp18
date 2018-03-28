e54e69adb6f3abae00f60e9023e7147448d2f3ac
Ethan Thibault <wickedlystudios@gmail.com>
2018-03-28 16:09:43 -0400

    Implemented Wolf Audio Behaviors
    - Wolf Aggro, Wolf Attack, Wolf Lunge all with proper events and listeners

--- paranormal-hunter/Assets/Audio/AudioTestingInput.cs
+++ paranormal-hunter/Assets/Audio/AudioTestingInput.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioTestingInput : MonoBehaviour {
+	
+	#region Variables
 	[Header("Test Inputs - Music")]
 	public KeyCode MusicStop = KeyCode.Q;
-	
+
 	[Header("Test Inputs - Player")]
 	public KeyCode Footstep = KeyCode.F;
 	public KeyCode PlayerHit = KeyCode.H;
@@ -15,12 +17,16 @@ public class AudioTestingInput : MonoBehaviour {
 	[Header("Test Inputs - Luger")]
 	public KeyCode PlayerDrawLuger = KeyCode.D;
 	public KeyCode PlayerLugerShot = KeyCode.L;
-	
+
 	[Header("Test Inputs - Mob")]
+	public KeyCode WolfAggro = KeyCode.E;
 	public KeyCode WolfAttack = KeyCode.W;
+	public KeyCode WolfLunge = KeyCode.R;
 
 	/* Private */
 	bool ExpoMusicPlaying = true;
+	
+	#endregion
 	
 	void Update () {
 
@@ -65,60 +71,111 @@ public class AudioTestingInput : MonoBehaviour {
 		}
 
 		/* Mob Events */
+		if(Input.GetKeyDown(WolfAggro)){
+			PlayWolfAggro();
+		}
 		if(Input.GetKeyDown(WolfAttack)){
 			PlayWolfAttack();
 		}
+		if(Input.GetKeyDown(WolfLunge)){
+			PlayWolfLungeAttack();
+		}
 	}
 	
-	/***** SFX Events *****/
-	/* All SFX should be posted with a reference to their parent object,
-		So that Fabric can track their 3D position and instances.
-		If this isn't possible, just remove the ", gameObject" */
+/* All SFX should be posted with a reference to their parent object,
+	So that Fabric can track their 3D position and instances.
+	If this isn't possible, just remove the ", gameObject" */
 
-	/* Player Events */
+#region Player-SFX-Events
+/***** Player SFX Events *****/
+
+/*** Player Character Events ***/ 
+	
 	// Player footsteps
-	void PlayFootsteps(){
-		Fabric.EventManager.Instance.PostEvent("Footstep", gameObject);
-	}
+		void PlayFootsteps()
+		{
+			Fabric.EventManager.Instance.PostEvent("Footstep", gameObject);
+		}
+		
 	// For when the player gets hit
-	void PlayPlayerHit(){
-		Fabric.EventManager.Instance.PostEvent("Player Hit", gameObject);
-	}
+	// Later we will have different versions for the amount of damage the player takes
+		void PlayPlayerHit()
+		{
+			Fabric.EventManager.Instance.PostEvent("Player Hit", gameObject);
+		}
 
-	/* Sword Events */
-	void PlayPlayerSwordSwing(){
-		Fabric.EventManager.Instance.PostEvent("Player Sword Swing", gameObject);
-	}
-	void PlayPlayerSwordHit(){
-		Fabric.EventManager.Instance.PostEvent("Player Sword Hit", gameObject);
-	}
+/*** Sword Events ***/
+	
+	// For *every time* the player swings their sword
+		void PlayPlayerSwordSwing()
+		{
+			Fabric.EventManager.Instance.PostEvent("Player Sword Swing", gameObject);
+		}
 
-	/* Ranged Weapon Events */
-	// Luger Shot
-	void PlayDrawLuger(){
-		Fabric.EventManager.Instance.PostEvent("Player Draw Luger", gameObject);
-	}
-	void PlayLugerShot(){
-		// Currently 1 shot pitch modulated, will fix later to be random container
-		Fabric.EventManager.Instance.PostEvent("Player Luger Shot", gameObject);
-	}
+	// When the player swings and *hits* something and does damage
+		void PlayPlayerSwordHit()
+		{
+			Fabric.EventManager.Instance.PostEvent("Player Sword Hit", gameObject);
+		}
 
-	/* Mob Events */
-	void PlayWolfAttack(){
-		// Currently has 1 attack modulated multiple times, will fix later
-		Fabric.EventManager.Instance.PostEvent("Wolf Attack", gameObject);
-	}
+/*** Ranged Weapon Events ***/
+	
+	// For when the player switches to the Luger weapon
+		void PlayDrawLuger()
+		{
+			Fabric.EventManager.Instance.PostEvent("Player Draw Luger", gameObject);
+		}
 
-	/***** Music Events *****/
+	// Luger Shot (Triggers 3 different audio sources at once)
+		void PlayLugerShot()
+		{
+			// Currently 1 shot pitch modulated, will fix later to be random container
+			Fabric.EventManager.Instance.PostEvent("Player Luger Shot", gameObject);
+		}
+
+#endregion
+
+#region Mob-SFX-Events
+/***** Mob SFX Events *****/
+	// For the associated monsters
+
+/*** Wolf Events ***/
+	// All wolf events posted from the wolf instance
+
+	// Whe the player is detected by a Wolf
+		void PlayWolfAggro()
+		{
+			Fabric.EventManager.Instance.PostEvent("Wolf Aggro", gameObject);
+		}
+	
+	// For when the wolf attacks
+		void PlayWolfAttack()
+		{
+			Fabric.EventManager.Instance.PostEvent("Wolf Attack", gameObject);
+		}
+	
+	// For the wolf's lunge attack
+		void PlayWolfLungeAttack()
+		{
+			Fabric.EventManager.Instance.PostEvent("Wolf Lunge Attack", gameObject);
+		}
+
+#endregion
+
+#region Music-Events
+/***** Music Events *****/
 	// Music Events don't need to pass a gameObject because they are entirely 2D.
 
 	// Transitions music from Exploration to Combat
-	void ExpotoCombatMusic(){
-		Fabric.EventManager.Instance.PostEvent("Expo to Combat Music");
-	}
+		void ExpotoCombatMusic()
+		{
+			Fabric.EventManager.Instance.PostEvent("Expo to Combat Music");
+		}
 	
 	//Transitions music from Combat to Exploration 
-	void CombattoExpoMusic(){
-		Fabric.EventManager.Instance.PostEvent("Combat to Expo Music");
-	}
+		void CombattoExpoMusic()
+		{
+			Fabric.EventManager.Instance.PostEvent("Combat to Expo Music");
+		}
 }
+#endregion
